ac94475b6a44df8942ac015ebb8ad0670763de8c
MalayaliRobz <robinphilipthomas@outlook.in>
2020-05-15 00:29:30 +0530

    update 1

--- content/script.js
+++ content/script.js
@@ -1,45 +1,106 @@
 let siteName = window.location.origin
 
-function findNumberDoms(domElements) {
+let isExtensionEnabled;
+let differenceLimit;
+
+let isExtensionEnabledPromise = new Promise((resolve) => chrome.storage.sync.get(['isExtensionEnabled'], (result) => resolve(result.isExtensionEnabled)));
+let differenceLimitPromise = new Promise((resolve) => chrome.storage.sync.get(['differenceLimit'], (result) => resolve(result.differenceLimit)));
+
+async function main() {
+  isExtensionEnabled = await isExtensionEnabledPromise;
+  differenceLimit = await differenceLimitPromise;
+
+  differenceLimit = differenceLimit || 4;  
+  isExtensionEnabled = isExtensionEnabled != undefined ? isExtensionEnabled : true;
+  
+  if (isExtensionEnabled) {    
+    let domElements;
+    if (origin.includes('amazon')) {
+      domElements = document.querySelectorAll("[class*='price'],[id*='price']");
+      // domElements = document.querySelectorAll(".a-price-whole");
+    } else {
+      // domElements = document.querySelectorAll("body *");
+    }
+
+    findNumberDoms(domElements);
+  }
+
+  registerMutationObserver(mutationObserver);
+}
+
+main();
+
+function findNumberDoms(domElements = []) {
   domElements.forEach(element => {
     if (element.hasChildNodes()) {
       let elements = element.childNodes;
       findNumberDoms(elements)
     } else {
-      let numberString = element.textContent.trim().replace(/[\s,]/g, "");
-      if(/\d+/.test(numberString)) {
-        let currencySymbol = (/\d*([^0-9]*)\d*/.exec(numberString) || [[]])[1];
+      let numberString = (element.wholeText || '').trim().replace(/[^.\d]/g, "");
+      if(/\d+.?\d*\s?$/.test(element.wholeText) &&!!parseFloat(numberString) && !isNaN(numberString)) {
+        let textContent = element.parentElement.textContent;
+        let newValue = roundNumber(numberString, differenceLimit);
 
-        let number = numberString.replace(currencySymbol, '');
-        if(!isNaN(number)) {
-          element.textContent = `${currencySymbol} ${roundNumber(parseFloat(number))}`;
+        if (parseFloat(numberString) !== parseFloat(newValue)) {
+          let parentElement = element.parentElement;
+          parentElement.textContent = textContent.replace(/\d+.?\d*/, newValue);
+
+          parentElement.setAttribute('aria-label', textContent);
+          parentElement.setAttribute('role', 'tooltip');
+          parentElement.setAttribute('data-microtip-position', 'right');
         }
+        
       }
     }
   });
 }
 
-function roundNumber(number) {
-  if (parseInt(number * 100) % 10 >= 9) {
-    number += .01;
+function roundNumber(number, differenceLimit) {
+  let origNumber = number;
+  number = parseFloat(number);
+  differenceLimit = parseFloat(differenceLimit);
+  let nextWholeNumber = findNextWholeNumber(number);
+
+  let difference  = nextWholeNumber - number;
+
+  if (difference <= differenceLimit) {
+    return returnNumberWithPointer(nextWholeNumber, origNumber);
+  } else {
+    return returnNumberWithPointer(number, origNumber);
   }
-  if (parseInt(number * 10) % 10 >= 9) {
-    number += .1;
-  }
-  if (parseInt(number) % 10 >= 9) {
-    number += 1;
-  }
-  if (parseInt(number/10) % 10 >= 9) {
-    number += 10;
-  }
-  return number.toFixed(2);
 }
 
-let domElements;
-if (origin.includes('amazon')) {
-  domElements = document.querySelectorAll("[class*='price'],[id*='price']");
-} else {
-  domElements = document.querySelectorAll("body *");
+function returnNumberWithPointer(number, origNumber) {
+  if (number == parseInt(origNumber)) {
+    if (String(origNumber).includes('.')) {
+      return number.toFixed(2);
+    }
+  }
+  return number;
 }
 
-findNumberDoms(domElements);
+function findNextWholeNumber(number) {
+  return Math.ceil(number / 10) * 10;
+}
+
+const registerMutationObserver = (mutationObserver) => {
+  mutationObserver.observe(document.body, {childList: true, subtree: true});
+};
+
+const mutationObserver = new MutationObserver(function (mutationRecords) {
+  mutationObserver.disconnect();
+  mutationRecords.forEach(function (mutationRecord) {
+    if (mutationRecord.type === 'childList') {
+      mutationRecord.addedNodes.forEach(function (node) {
+        if(node.nodeName != 'SCRIPT' && /\d+/.test(node.textContent)) {
+          let domElements = node.querySelectorAll && node.querySelectorAll("[class*='price'],[id*='price']");
+
+          if (isExtensionEnabled) {
+            findNumberDoms(domElements);
+          }
+        }
+      });
+    }
+  });
+  registerMutationObserver(mutationObserver);
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,88 @@
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
+let differenceLimitInput = document.querySelector('#differenceLimitInput');
+let pauseBtn = document.querySelector('#pauseBtn');
+let isActive;
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get(['differenceLimit'], (result) => {
+  updateExamples(result.differenceLimit);
 });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+
+differenceLimitInput.onchange = function(event) {
+  let value = event.target.value;
+  chrome.storage.sync.set({
+    'differenceLimit': value
   });
-};
+  updateExamples(value);
+  showRefresh();
+}
+
+chrome.storage.sync.get(['isExtensionEnabled'], ({ isExtensionEnabled }) => {
+  updateActiveState(isExtensionEnabled);
+  isActive = isExtensionEnabled;
+});
+
+chrome.storage.sync.get(['differenceLimit'], ({ differenceLimit }) => {
+  differenceLimitInput.value = differenceLimit;
+});
+
+
+pauseBtn.onclick = function() {
+  isActive = !isActive;
+  chrome.storage.sync.set({
+    'isExtensionEnabled': isActive
+  });
+  updateActiveState(isActive);
+  showRefresh();
+}
+
+
+
+function updateExamples(differenceLimit) {
+  [1,2,3,4,5,6].forEach(no => {
+    let selector = `#example${no}`;
+    let element = document.querySelector(selector);
+
+    let inputNo = parseFloat(element.querySelector("[name=input]").textContent.replace(/[^.\d]/, ''));
+    let outputNo = roundNumber(inputNo, differenceLimit);
+
+    element.querySelector("[name=output]").textContent = `$${outputNo}`;
+
+    if (inputNo !== outputNo) {
+      element.querySelector("[name=output]").classList.add("strong");
+    } else {
+      element.querySelector("[name=output]").classList.remove("strong");
+    }
+  })
+}
+
+function roundNumber(number, differenceLimit) {
+  differenceLimit = parseFloat(differenceLimit);
+  let nextWholeNumber = findNextWholeNumber(number);
+
+  let difference  = nextWholeNumber - number;
+
+  if (difference <= differenceLimit) {
+    return nextWholeNumber;
+  } else {
+    return number;
+  }
+}
+
+function findNextWholeNumber(number) {
+  return Math.ceil(number / 10) * 10;
+}
+
+function updateActiveState(state) {
+  let pauseElemet = document.querySelector("#pauseBtn");
+  if (state) {
+    pauseElemet.textContent = "Pause";
+  } else {
+    pauseElemet.textContent = "Resume";
+  }
+}
+
+function showRefresh() {
+  document.querySelector("#contentChangedInfo").classList.remove("hide");
+}
